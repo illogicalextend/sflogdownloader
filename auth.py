df6c32c76ffba1f403bd543985db3bdca075306a
Ryan Brailey <illogicalextend@github.com>
2017-03-11 08:23:16 +0000

    Big refactor in to modules

---
+++ auth.py
@@ -0,0 +1,13 @@
+
+
+
+from simple_salesforce import Salesforce
+
+def authSalesforce():
+    with open('credentials.txt') as f:
+        lines = f.read().splitlines()
+        sfUsername = lines[0]
+        sfPassword = lines[1]
+        sfToken = lines[2]
+        sf = Salesforce(username='%s' % sfUsername, password='%s' % sfPassword, security_token='%s' % sfToken)
+        return sf
---
+++ getcases.py
@@ -0,0 +1,12 @@
+
+
+from auth import authSalesforce
+
+def getSalesforceCases(LogDestBase):
+     print "Destination: {}".format(LogDestBase)
+     print "Checking for available logs.."
+     #caseListResult = "SELECT CaseNumber, Status, Case_External_ID__c, LogLocationFTPURL__c FROM Case where (Status != 'closed' and Status != 'Closed No Response' and Status != 'Merged' and Status != 'Junk' and Status != 'Closed by Customer' and Status != 'Closed In KB' and Status != 'Call No-Answer') and (OwnerId = '00532000004yymVAAQ' or OwnerId = '00532000003hdv9AAA' or OwnerId = '00532000003i25wAAA' or OwnerId = '00532000004ytb3AAA' or OwnerId = '00532000004yymaAAA' or OwnerId = '00532000004z91pAAA' or OwnerId = '00532000004zGNxAAM' or OwnerId = '00532000004zXpjAAE' or OwnerId = '00532000004zcz0AAA' or OwnerId = '00532000004zkB3AAI' or OwnerId = '00532000005NPnsAAG' or OwnerId = '00532000005UlUNAA0' or OwnerId = '00532000005UlUSAA0' or OwnerId = '00532000005UlUXAA0' or OwnerId = '00532000005V37vAAC' or OwnerId = '005600000029jF0AAI' or OwnerId = '00560000002AsvZAAS' or OwnerId = '00560000003X9XRAA0' or OwnerId = '00560000003XehPAAS') ORDER BY CaseNumber ASC"
+     caseListResult = "SELECT CaseNumber, Status, Case_External_ID__c, LogLocationFTPURL__c FROM Case where (Status != 'closed' and Status != 'Closed No Response' and Status != 'Merged' and Status != 'Junk' and Status != 'Closed by Customer' and Status != 'Closed In KB' and Status != 'Call No-Answer') and (OwnerId = '00532000004yymVAAQ') ORDER BY CaseNumber ASC"
+     query = authSalesforce().query(caseListResult)
+     records = query['records']
+     return records
